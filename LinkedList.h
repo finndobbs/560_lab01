a7592a7f0ada641970da34ab724405c2feed618d
Finn Dobbs <finndobbs@ku.edu>
2020-01-28 11:43:54 -0600

    Add LinkedList.h with public methods

---
+++ LinkedList.h
@@ -0,0 +1,19 @@
+#include <string>
+
+class LinkedList{
+public:
+    LinkedList();
+    ~LinkedList();
+
+    void IsEmpty();
+    void Length();
+    void Insert(x);
+    void Delete(x);
+    void DeleteDuplicates();
+    int  Find(x);
+    int  FindNext(x);
+    std::string Print();
+    std::string ReverseList();
+    std::string PrintAt(x);
+    void Exit();
+}
